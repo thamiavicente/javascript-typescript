b4f2f0fd4f53299f973b0febc7d1bb18cd71d3c4
Thamiris Vicente <thamiavicente@gmail.com>
2025-07-07 18:27:00 +0200

    Create seed file

--- Projetos/TDD-e-BDD/seed/index.js
+++ Projetos/TDD-e-BDD/seed/index.js
@@ -1,12 +1,14 @@
 const { faker } = require('@faker-js/faker')
+
 const { join } = require('path')
-const seederBaseFolder = join(_dirname, "../", "database")
+const seederBaseFolder = join(__dirname, "../", "database")
+const { writeFile } = require('fs/promises')
 
 const Car = require('../src/entities/car')
 const CarCategory = require('../src/entities/carCategory')
 const Customer = require('../src/entities/customer')
 
-const CARS_AMOUNT = 2
+const ITEMS_AMOUNT = 2
 
 const carCategory = new CarCategory({
     id: faker.string.uuid(),
@@ -15,7 +17,9 @@ const carCategory = new CarCategory({
     price: faker.finance.amount(20, 100)
 })
 
-for(let quantity = 0; quantity < CARS_AMOUNT; quantity++) {
+const cars = []
+const customers = []
+for(let quantity = 0; quantity < ITEMS_AMOUNT; quantity++) {
     const car = new Car({
         id: faker.string.uuid(),
         name: faker.vehicle.model(),
@@ -23,4 +27,21 @@ for(let quantity = 0; quantity < CARS_AMOUNT; quantity++) {
         available: true,
         gasAvailable: true
     })
+    carCategory.carIds.push(car.id)
+    cars.push(car)
+
+    const customer = new Customer({
+        id: faker.string.uuid(),
+        name: faker.person.fullName(),
+        age: faker.number.int({ min: 18, max: 60 })
+    })
+    customers.push(customer)
 }
+
+const write = (filename, data) => writeFile(join(seederBaseFolder, filename), JSON.stringify(data)) 
+
+;(async () => {
+    await write('cars.json', cars)
+    await write('car-category.json', [carCategory])
+    await write('customer.json', customers)
+})()
